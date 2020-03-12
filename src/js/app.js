d7334a815327ffb3912ecfd61dd8d65b84d477f6
Igor Korenets <vik753@gmail.com>
2020-03-12 13:09:41 +0200

    step1 done

--- src/js/app.js
+++ src/js/app.js
@@ -4,6 +4,13 @@ import "../css/style.css";
 import UI from "./config/ui.config";
 import { validate } from "./helpers/validate";
 import { showInputError, removeInputError } from "./views/form";
+import { login } from "./services/auth.service";
+import { notify } from "./views/notifications";
+
+/*
+ * l: denis.m.pcspace@gmail.com
+ * p: dmgame12345
+ */
 
 const { form, inputEmail, inputPassword } = UI;
 const inputs = [inputEmail, inputPassword];
@@ -13,20 +20,31 @@ form.addEventListener("submit", e => {
   e.preventDefault();
   onSubmit();
 });
-inputs.forEach(el => {
-  el.addEventListener('focus', () => {
-    removeInputError(el);
-  })
-});
+inputs.forEach(el => el.addEventListener("focus", () => removeInputError(el)));
 
 // Handlers
-function onSubmit() {
+async function onSubmit() {
   const isValidForm = inputs.every(el => {
     const isValidInput = validate(el);
-    if (!isValidInput) {
+    if (!isValidInput && !el.classList.contains("is-invalid")) {
       showInputError(el);
     }
     return isValidInput;
   });
-  console.log(isValidForm);
+
+  if (!isValidForm) return;
+
+  try {
+
+    let response = await login(inputEmail.value, inputPassword.value);
+    const { error, auth, token, id } = response;
+    form.reset();
+    notify({
+      msg: `Login success! Your id: ${id}`,
+      className: "alert-success",
+    });
+  } catch (error) {
+    // console.dir(error)
+    notify({ msg: `${error.response.data.message}`, className: "alert-danger" });
+  }
 }
---
+++ src/js/services/auth.service.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+import API_ENV from "../config/api.config";
+
+/**
+ * Function login. Make login request to API
+ * @param {String} email
+ * @param {String} password
+ */
+export async function login(email, password) {
+  try {
+    const response = await axios.post(
+      `${API_ENV.apiUrl}/auth/login`,
+      JSON.stringify({ email, password }),
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    console.log(response);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+    return Promise.reject(error);
+  }
+}
